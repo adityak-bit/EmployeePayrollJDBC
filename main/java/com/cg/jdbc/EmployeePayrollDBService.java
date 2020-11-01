2760a1e925b3a1907bddec0d8e6c9afa41a9075a
adityak-bit <adityajsr1998@gmail.com>
2020-11-01 11:33:44 +0530

    [ADITYA] Refactored the code to close the Connection

--- main/java/com/cg/jdbc/EmployeePayrollDBService.java
+++ main/java/com/cg/jdbc/EmployeePayrollDBService.java
@@ -25,15 +25,14 @@ public class EmployeePayrollDBService {
 	public List<EmployeePayrollData> readData() {
 		String sql = "SELECT * FROM employee_payroll;";
 		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-		try {
-			Connection connection = this.getConnection();
+		try (Connection connection = this.getConnection()){
 			Statement statement = connection.createStatement();
-			ResultSet result = statement.executeQuery(sql);
-			while(result.next()) {
-				int id =  result.getInt("id");
-				String name = result.getString("name");
-				double basic_pay = result.getDouble("basic_pay");
-				LocalDate startDate = result.getDate("start").toLocalDate();
+			ResultSet resultSet = statement.executeQuery(sql);
+			while(resultSet.next()) {
+				int id =  resultSet.getInt("id");
+				String name = resultSet.getString("name");
+				double basic_pay = resultSet.getDouble("basic_pay");
+				LocalDate startDate = resultSet.getDate("start").toLocalDate();
 				employeePayrollList.add(new EmployeePayrollData(id, name, basic_pay, startDate));
 			}
 		} catch (SQLException e) {
