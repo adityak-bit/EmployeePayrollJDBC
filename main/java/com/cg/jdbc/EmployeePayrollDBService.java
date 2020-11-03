e7bc2be2fc4a9a016c8228723abdc08900446ec9
adityak-bit <adityajsr1998@gmail.com>
2020-11-03 12:42:17 +0530

    [ADITYA] Added : Refactored code as DRY was getting violated

--- main/java/com/cg/jdbc/EmployeePayrollDBService.java
+++ main/java/com/cg/jdbc/EmployeePayrollDBService.java
@@ -38,6 +38,15 @@ public class EmployeePayrollDBService {
 
 	public List<EmployeePayrollData> readData() {
 		String sql = "SELECT * FROM employee_payroll;";
+		return this.getEmployeeDataUsingDB(sql);
+	}
+	
+	public List<EmployeePayrollData> readData(LocalDate startDate, LocalDate endDate) {
+		String sql = String.format("SELECT * FROM employee_payroll where start between '%s' and '%s'", Date.valueOf(startDate), Date.valueOf(endDate));
+		return this.getEmployeeDataUsingDB(sql);
+	}
+
+	List<EmployeePayrollData> getEmployeeDataUsingDB(String sql){
 		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
 		try (Connection connection = this.getConnection()){
 			Statement statement = connection.createStatement();
@@ -49,19 +58,6 @@ public class EmployeePayrollDBService {
 		return employeePayrollList;
 	}
 	
-	public List<EmployeePayrollData> readData(LocalDate startDate, LocalDate endDate) {
-		String sql = String.format("SELECT * FROM employee_payroll where start between '%s' and '%s'", Date.valueOf(startDate), Date.valueOf(endDate));
-		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-		try (Connection connection = this.getConnection()){
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery(sql);
-			employeePayrollList = this.getEmployeePayrollData(resultSet);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return employeePayrollList;
-	}
-
 	public List<EmployeePayrollData> getEmployeePayrollData(String name) {
 		List<EmployeePayrollData> employeePayrollList =  null;
 		if(this.employeePayrollDataStatement == null)
